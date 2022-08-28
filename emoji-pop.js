cf2144a58975f0360bb02a70ae50829f63c64d54
ENSpunks <109005555+ENSpunks@users.noreply.github.com>
2022-08-28 19:38:03 -0400

    Add files via upload

--- emoji-pop.js
+++ emoji-pop.js
@@ -5,7 +5,7 @@ class Fountain {
     this.autoAddParticle = false;
     this.height = document.documentElement.clientHeight;
     this.sizes = [15, 20, 25, 35, 45];
-    this.variants = ["🐄", "🐄"];
+    this.variants = ["☠️", "☠️"];
     this.addHandlers();
     this.loop();
   }
@@ -13,9 +13,9 @@ class Fountain {
   loop() {
     if (this.autoAddParticle && this.particles.length < this.limit) {
       // if (document.getElementById("field-email") === document.activeElement) {
-      //   this.variants = ["🐄"];
+      //   this.variants = ["☠️"];
       // } else {
-      //   this.variants = ["🐄"];
+      //   this.variants = ["☠️"];
       // }
       this.createParticle();
     }
